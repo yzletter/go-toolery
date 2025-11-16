7f0e7b2dcf0d13794baa972d81af2b49a53ab86e
yzletter <yzletter@foxmail.com>
2025-11-16 15:06:14 +0800

    编写 Set 及对应单测

--- data_structure/setx/set.go
+++ data_structure/setx/set.go
@@ -1,3 +1,44 @@
 package setx
 
-// todo 手写集合
+// EmptyStruct 空结构体
+type EmptyStruct struct{}
+
+// Set 集合
+type Set[T comparable] map[T]EmptyStruct
+
+// NewSet 构造函数
+func NewSet[T comparable]() Set[T] {
+	return make(map[T]EmptyStruct)
+}
+
+// Insert 插入一个新元素
+func (hash Set[T]) Insert(val T) {
+	hash[val] = EmptyStruct{}
+}
+
+// Delete 删除一个元素
+func (hash Set[T]) Delete(val T) {
+	delete(hash, val)
+}
+
+// Exist 查询一个元素
+func (hash Set[T]) Exist(val T) bool {
+	_, ok := hash[val]
+	return ok
+}
+
+// Size 返回集合中元素个数
+func (hash Set[T]) Size() int {
+	return len(hash)
+}
+
+// Values 返回集合中所有元素的切片
+func (hash Set[T]) Values() []T {
+	ans := make([]T, hash.Size())
+	idx := 0
+	for v, _ := range hash {
+		ans[idx] = v
+		idx++
+	}
+	return ans
+}
---
+++ data_structure/setx/set_test.go
@@ -0,0 +1,28 @@
+package setx_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/yzletter/go-toolery/data_structure/setx"
+)
+
+func TestSet(t *testing.T) {
+	hash := setx.NewSet[int]()
+	for i := 0; i < 100; i++ {
+		hash.Insert(rand.Intn(10))
+	}
+
+	vals := hash.Values()
+	fmt.Println(vals)
+
+	idx := 0
+	for hash.Size() > 0 {
+		hash.Delete(vals[idx])
+		fmt.Println(hash.Values())
+		idx++
+	}
+}
+
+// go test -v ./data_structure/setx -run=^TestSet$ -count=1
