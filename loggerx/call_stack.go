65d9beec390afe55e0b7c06787a89302717c94ba
yzletter <yzletter@foxmail.com>
2025-12-09 15:44:22 +0800

    实现 Logger 并发打印堆栈信息

---
+++ loggerx/call_stack.go
@@ -0,0 +1,36 @@
+package loggerx
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+// 返回调用当前函数的堆栈信息
+func currentStack() string {
+	if _, file, line, ok := runtime.Caller(2); ok {
+		return fmt.Sprintf("%s:%d", shortFile(file), line)
+	} else {
+		return ""
+	}
+}
+
+// 避免路径过长, 只保留最后三节
+func shortFile(file string) string {
+	arr := strings.Split(file, "/")
+	if len(arr) > 3 {
+		arr = arr[len(arr)-3:]
+	}
+	return strings.Join(arr, "/")
+}
+
+func stackPath() []string {
+	rect := make([]string, 3)
+	for i := 3; i < 6; i++ {
+		_, file, line, ok := runtime.Caller(i)
+		if ok {
+			rect = append(rect, fmt.Sprintf("%s:%d", shortFile(file), line))
+		}
+	}
+	return rect
+}
--- loggerx/log.go
+++ loggerx/log.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 const (
@@ -14,15 +15,18 @@ const (
 )
 
 type Log struct {
-	logger   *log.Logger // 基本库日志
-	logFile  string      // 日志输出文件
-	logLevel int         // 日志等级
+	logger    *log.Logger // 基本库日志
+	logFile   string      // 日志输出文件
+	logOut    *os.File    // 日志输出文件句柄
+	logLevel  int         // 日志等级
+	writeLock sync.Mutex  // 锁
 }
 
 func NewLog(logFile string, logLevel int) *Log {
 	if file, err := os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o666); err == nil {
 		return &Log{
 			logger:   log.New(file, "", log.Ldate|log.Lmicroseconds),
+			logOut:   file,
 			logLevel: logLevel,
 		}
 	} else {
@@ -32,6 +36,7 @@ func NewLog(logFile string, logLevel int) *Log {
 
 func (log *Log) print(level int, content string) {
 	var prefix string
+
 	switch level {
 	case DebugLevel:
 		prefix = "DEBUG "
@@ -42,11 +47,16 @@ func (log *Log) print(level int, content string) {
 	case ErrorLevel:
 		prefix = "ERROR "
 	}
-	log.logger.Println(prefix + content)
+	
+	msg := prefix + " " + currentStack() + " " + content
+	log.logger.Println(msg)
 }
 
 func (log *Log) Debug(content string) {
 	if log.logLevel <= DebugLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		log.print(DebugLevel, content)
 	}
 
@@ -54,6 +64,9 @@ func (log *Log) Debug(content string) {
 
 func (log *Log) Debugf(format string, v ...any) {
 	if log.logLevel <= DebugLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		content := fmt.Sprintf(format, v...)
 		log.print(DebugLevel, content)
 	}
@@ -61,12 +74,18 @@ func (log *Log) Debugf(format string, v ...any) {
 
 func (log *Log) Warn(content string) {
 	if log.logLevel <= WarnLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		log.print(WarnLevel, content)
 	}
 }
 
 func (log *Log) Warnf(format string, v ...any) {
 	if log.logLevel <= WarnLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		content := fmt.Sprintf(format, v...)
 		log.print(WarnLevel, content)
 	}
@@ -74,12 +93,18 @@ func (log *Log) Warnf(format string, v ...any) {
 
 func (log *Log) Info(content string) {
 	if log.logLevel <= InfoLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		log.print(InfoLevel, content)
 	}
 }
 
 func (log *Log) Infof(format string, v ...any) {
 	if log.logLevel <= InfoLevel {
+		log.writeLock.Lock()
+
+		defer log.writeLock.Unlock()
 		content := fmt.Sprintf(format, v...)
 		log.print(InfoLevel, content)
 	}
@@ -87,13 +112,25 @@ func (log *Log) Infof(format string, v ...any) {
 
 func (log *Log) Error(content string) {
 	if log.logLevel <= ErrorLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		log.print(ErrorLevel, content)
+		for _, stk := range stackPath() {
+			_, _ = log.logOut.WriteString("\t" + stk + "\n")
+		}
 	}
 }
 
 func (log *Log) Errorf(format string, v ...any) {
 	if log.logLevel <= ErrorLevel {
+		log.writeLock.Lock()
+		defer log.writeLock.Unlock()
+
 		content := fmt.Sprintf(format, v...)
 		log.print(ErrorLevel, content)
+		for _, stk := range stackPath() {
+			_, _ = log.logOut.WriteString("\t" + stk + "\n")
+		}
 	}
 }
